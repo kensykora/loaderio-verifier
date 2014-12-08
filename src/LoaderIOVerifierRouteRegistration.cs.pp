f37d5a112c7eeeb7026675bd5c74c3bd7fcf3d3a
Ken Sykora <ken.sykora@gmail.com>
2014-12-08 17:11:15 -0600

    Ignoring route for installations

---
+++ src/LoaderIOVerifierRouteRegistration.cs.pp
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(
+    typeof($rootnamespace$.App_Start.LoaderIOVerifierRouteRegistration), "PreStart")]
+
+namespace $rootnamespace$.App_Start {
+    public static class LoaderIOVerifierRouteRegistration {
+        public static void PreStart() {
+            RouteTable.Routes.IgnoreRoute("loaderio-{key}.txt");
+        }
+    }
+}
